eb660a1655afce40321fbef0b7c37574daa6ccb8
ikusimakusi <ikerolaba@gmail.com>
2020-04-15 15:15:20 +0200

    elkartasun.js-ra mugituta

--- counter.js
+++
@@ -1,6 +0,0 @@
-var inv = setInterval(function() {
-    if(i < errekaduak)
-        document.getElementById("myTargetElement").innerHTML = ++i;
-    else
-        clearInterval(inv);
-}, 2000 / errekaduak);
