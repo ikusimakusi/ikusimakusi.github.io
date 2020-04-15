5aaf039ca26fe5960f195ed279b4613b5571c79b
ikusimakusi <ikerolaba@gmail.com>
2020-04-15 15:11:20 +0200

    datu formatu berria

--- auzo_datuak.js
+++ auzo_datuak.js
@@ -1,87 +1,100 @@
 
 var errekaduak=364;
-var datuak= [ {
+var datuak= [
+  {
+    "id": "1",
+    "auzoa" : "Deustu",
+    "bolondresak": "120",
+    "lat":"43.2699",
+    "lng":"-2.9500"
+  },
+  {
+    "id": "2",
+    "auzoa" : "Irala",
+    "bolondresak": "48",
+    "lat":"43.2504",
+    "lng":"-2.9400"
+  },
+  {  "id": "3",
+    "auzoa" : "San Francisco",
+    "bolondresak": "90",
+    "lat":"43.2556",
+    "lng":"-2.9275"
+  },
+  { "id": "4",
+    "auzoa" : "San Adrian - Miribilla",
+    "bolondresak": "65",
+    "lat":"43.2492",
+    "lng":"-2.930"
+  },
+  {
+    "id": "5",
+    "auzoa" : "San Inazio - Ibarrekolanda - Elorrieta",
+    "bolondresak": "70",
+    "lat":"43.2784",
+    "lng":"-2.9615"
+  },
+  {
+    "id": "6",
+    "auzoa" : "Arangoiti",
+    "bolondresak": "26",
+    "lat":"43.2745",
+    "lng":"-2.9489"
+  },
+  {
+      "id": "7",
+    "auzoa" : "Errekalde",
+    "bolondresak": "64",
+    "lat":"43.2485",
+    "lng":"-2.9468"
+  },
+
+   {
+  "id": "8",
   "auzoa" : "Santutxu",
   "bolondresak": "70",
   "lat":"43.2515",
   "lng":"-2.9146"
 },
-{
-  "auzoa" : "Deustu",
-  "bolondresak": "120",
-  "lat":"43.2699",
-  "lng":"-2.9500"
-},
-{
-  "auzoa" : "Irala",
-  "bolondresak": "48",
-  "lat":"43.2504",
-  "lng":"-2.9400"
-},
-{
-  "auzoa" : "San Francisco",
-  "bolondresak": "90",
-  "lat":"43.2556",
-  "lng":"-2.9275"
-},
-{
+
+
+{  "id": "9" ,
   "auzoa" : "Zabala",
   "bolondresak": "20",
   "lat":"43.2542",
   "lng":"-2.9328"
 },
-{
+{ "id": "10" ,
   "auzoa" : "Bilbo Zaharra",
   "bolondresak": "48",
   "lat":"43.2536",
   "lng":"-2.9245"
 },
-{
+{"id": "11" ,
   "auzoa" : "Otxarkoaga",
   "bolondresak": "49",
   "lat":"43.2587",
   "lng":"-2.9002"
 },
-{
+{"id": "12" ,
   "auzoa" : "Alde Zaharra",
   "bolondresak": "180",
   "lat":"43.2573",
   "lng":"-2.9240"
 },
-{
+{"id": "13" ,
   "auzoa" : "Uribarri",
   "bolondresak": "156",
   "lat":"43.26682",
   "lng":"-2.9233"
 },
-{
+{"id": "14" ,
   "auzoa" : "Indautxu - Basurtu",
   "bolondresak": "111",
   "lat":"43.26061",
   "lng":"-2.9412"
-},
-{
-  "auzoa" : "San Adrian - Miribilla",
-  "bolondresak": "65",
-  "lat":"43.2492",
-  "lng":"-2.930"
-},
-{
-  "auzoa" : "San Inazio - Ibarrekolanda - Elorrieta",
-  "bolondresak": "70",
-  "lat":"43.2784",
-  "lng":"-2.9615"
-},
-{
-  "auzoa" : "Arangoiti",
-  "bolondresak": "26",
-  "lat":"43.2745",
-  "lng":"-2.9489"
-},
-{
-  "auzoa" : "Errekalde",
-  "bolondresak": "64",
-  "lat":"43.2485",
-  "lng":"-2.9468"
 }
+
+
+
 ];
